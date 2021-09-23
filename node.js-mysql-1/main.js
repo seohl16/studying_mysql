c0924f4143aad5bf9ad443ab1fdee0c666dc07f0
selim <selim@student.42seoul.kr>
2021-09-23 19:53:28 +0900

    청소3 var db deleted

--- node.js-mysql-1/main.js
+++ node.js-mysql-1/main.js
@@ -1,8 +1,5 @@
 var http = require('http');
 var url = require('url');
-var qs = require('querystring');
-var template = require('./lib/template.js');
-var db = require('./lib/db');
 var topic = require('./lib/topic');
 
 var app = http.createServer(function(request,response){
