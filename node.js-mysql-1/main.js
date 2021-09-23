df963ce12b18f4887e5946840aca172c3ac8179d
selim <selim@student.42seoul.kr>
2021-09-23 14:12:21 +0900

    var db mysql createConnection

--- node.js-mysql-1/main.js
+++ node.js-mysql-1/main.js
@@ -5,6 +5,15 @@ var qs = require('querystring');
 var template = require('./lib/template.js');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
+var mysql = require('mysql');
+var db = mysql.createConnection({
+  host:'localhost', 
+  user:'root', 
+  password:'', 
+  database: 'opentutorials'
+});
+
+db.connect();
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -12,17 +21,22 @@ var app = http.createServer(function(request,response){
     var pathname = url.parse(_url, true).pathname;
     if(pathname === '/'){
       if(queryData.id === undefined){
-        fs.readdir('./data', function(error, filelist){
-          var title = 'Welcome';
-          var description = 'Hello, Node.js';
-          var list = template.list(filelist);
-          var html = template.HTML(title, list,
-            `<h2>${title}</h2>${description}`,
-            `<a href="/create">create</a>`
-          );
+        // fs.readdir('./data', function(error, filelist){
+        //   var title = 'Welcome';
+        //   var description = 'Hello, Node.js';
+        //   var list = template.list(filelist);
+        //   var html = template.HTML(title, list,
+        //     `<h2>${title}</h2>${description}`,
+        //     `<a href="/create">create</a>`
+        //   );
+        //   response.writeHead(200);
+        //   response.end(html);
+        // });
+        db.query(`SELECT * FROM topic`, function(err, topics) {
+          console.log(topics);
           response.writeHead(200);
-          response.end(html);
-        });
+          response.end('Success');
+        })
       } else {
         fs.readdir('./data', function(error, filelist){
           var filteredId = path.parse(queryData.id).base;
